bfc2a1375c4e6c458207ec68e0935b7cc344d1a0
Ajas <ajasashraf7010@gmail.com>
2023-01-10 19:23:02 +0530

    atlas added

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
 
 module.exports.connect=function(){
-    mongoose.connect('mongodb://localhost:27017/Bikevio',()=>console.log('Connected to database'));
+    mongoose.connect('mongodb+srv://Bikevio:1234@cluster0.vxhj7hj.mongodb.net/cluster0?retryWrites=true&w=majority',()=>console.log('Connected to database'));
 }
